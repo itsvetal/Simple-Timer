3282b17b3428e3733f264ebb9913b63665c8d242
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-08-27 15:19:28 +0300

    Add library the moment to the project V1.2

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+import moment from "moment";
+
+'use strict'
+
+
+//const container = document.querySelector('.container');
+console.log(moment().second());
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   // Webpack needs to know where to start the bundling process,
   // so we define the main JS and Sass files, both under
   // the './src' directory
-  entry: ['./src/scripts/script.js', './src/styles/main.scss'],
+  entry: ['./src/scripts/main.js', './src/styles/main.scss'],
 
   // This is where we define the path where Webpack will place
   // the bundled JS file
