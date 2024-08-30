23c1908590f7a46ff4eacbe8d9cf25dbca94f167
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-08-30 18:40:08 +0300

    Connect moment library and moment-timer plugin. Work on the timer V1.3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,30 @@
-import moment from "moment";
-
 'use strict'
 
+import moment from "moment";
+import "moment-timer";
 
-//const container = document.querySelector('.container');
-console.log(moment().second());
+
+console.log(typeof moment.duration.fn.timer === 'function');
+const label = document.getElementById("label");
+const element = document.getElementById('timer');
+
+const timer = moment.duration(2, 'minutes').timer({
+    loop: false,
+    start: true
+}, () => {
+    label.innerText = 'Час вийшов!';
+    element.remove();
+});
+
+
+const interval = setInterval(() => {
+    const remainingTime = moment.duration(timer.getRemainingDuration());
+    const minutes = Math.floor(remainingTime.asMinutes());
+    const seconds = ((remainingTime.asSeconds()) % 60).toFixed(0);
+    if (remainingTime.asSeconds() < 0) {
+        clearInterval(interval);
+    } else {
+        label.innerText = 'Залишилось';
+        element.innerText = `${minutes}:${seconds.padStart(2, '0')}`;
+    }
+}, 1000);
